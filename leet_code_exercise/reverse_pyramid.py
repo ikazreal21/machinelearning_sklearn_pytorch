669fe600c4048f4f321cf3e579cad0aa19084da2
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-11-17 00:05:38 +0800

    Added Leet code

---
+++ leet_code_exercise/reverse_pyramid.py
@@ -0,0 +1,9 @@
+def reverse_number():
+    rows = 5
+    for i in range(0, rows + 1):
+        for j in range(rows - i, 0, -1):
+            print(j, end=" ")
+        print("\r")
+
+
+reverse_number()
