4f1087fa57f9ed5d9badf65a7b048e4bd213015a
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-11-20 00:00:25 +0800

    Updated Postgres Functions to a Class

--- leet_code_exercise/postgresql_CRUD.py
+++ leet_code_exercise/postgresql_CRUD.py
@@ -1,88 +1,89 @@
 import psycopg2
 
 
-def ConnectDB(database, user, password):
-    con = psycopg2.connect(
-        database=database,
-        user=user,
-        password=password,
-        host="127.0.0.1",
-    )
-    print("Database opened successfully")
-    return con
+class PostgresDriver:
+    def __init__(self, database, user, password):
+        self.database = database
+        self.user = user
+        self.password = password
 
+    def connect_database(self, database, user, password):
+        con = psycopg2.connect(
+            database=database,
+            user=user,
+            password=password,
+            host="127.0.0.1",
+        )
+        print("Database opened successfully")
+        return con
 
-def CreateTable(query):
-    con = ConnectDB("postgres", "postgres", "zakizakizaki")
-    cur = con.cursor()
-    cur.execute(query)
-    print("Table created successfully")
+    def create_table(self, query):
+        con = self.connect_database(self.database, self.user, self.password)
+        cur = con.cursor()
+        cur.execute(query)
+        print("Table created successfully")
 
-    con.commit()
-    con.close()
+        con.commit()
+        con.close()
 
+    def insert_data(self, data):
+        con = self.connect_database(self.database, self.user, self.password)
+        cur = con.cursor()
+        for i in data:
+            cur.execute(i)
 
-def InsertData(data):
-    con = ConnectDB("postgres", "postgres", "zakizakizaki")
-    cur = con.cursor()
-    for i in data:
-        cur.execute(i)
+        con.commit()
+        print("Record inserted successfully")
+        con.close()
 
-    con.commit()
-    print("Record inserted successfully")
-    con.close()
+    def retrieve_data(self, query):
+        con = self.connect_database(self.database, self.user, self.password)
+        cur = con.cursor()
+        cur.execute(query)
+        rows = cur.fetchall()
 
+        for row in rows:
+            print("ADMISSION =", row[0])
+            print("NAME =", row[1])
+            print("AGE =", row[2])
+            print("COURSE =", row[3])
+            print("DEPARTMENT =", row[4], "\n")
 
-def RetrieveData(query):
-    con = ConnectDB("postgres", "postgres", "zakizakizaki")
-    cur = con.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
+        print("Operation done successfully")
+        con.close()
 
-    for row in rows:
-        print("ADMISSION =", row[0])
-        print("NAME =", row[1])
-        print("AGE =", row[2])
-        print("COURSE =", row[3])
-        print("DEPARTMENT =", row[4], "\n")
+    def update_data(self, data, query):
+        con = self.connect_database(self.database, self.user, self.password)
+        cur = con.cursor()
+        for i in data:
+            cur.execute(i)
+        con.commit()
+        print("Total updated rows:", cur.rowcount)
+        RetrieveData(query)
+        print("Operation done successfully")
+        con.close()
 
-    print("Operation done successfully")
-    con.close()
-
-
-def UpdateData(data, query):
-    con = ConnectDB("postgres", "postgres", "zakizakizaki")
-    cur = con.cursor()
-    for i in data:
-        cur.execute(i)
-    con.commit()
-    print("Total updated rows:", cur.rowcount)
-    RetrieveData(query)
-    print("Operation done successfully")
-    con.close()
-
-
-def DeleteData(data, query):
-    con = ConnectDB("postgres", "postgres", "zakizakizaki")
-    cur = con.cursor()
-    for i in data:
-        cur.execute(i)
-    con.commit()
-    con.commit()
-    print("Total deleted rows:", cur.rowcount)
-    RetrieveData(query)
-    print("Deletion successful")
-    con.close()
+    def delete_data(self, data, query):
+        con = self.connect_database(self.database, self.user, self.password)
+        cur = con.cursor()
+        for i in data:
+            cur.execute(i)
+        con.commit()
+        con.commit()
+        print("Total deleted rows:", cur.rowcount)
+        RetrieveData(query)
+        print("Deletion successful")
+        con.close()
 
 
 table_creation = """CREATE TABLE STUDENT
-	      (
-	      ADMISSION INT PRIMARY KEY     NOT NULL,
-	      NAME           TEXT    NOT NULL,
-	      AGE            INT     NOT NULL,
-	      COURSE        CHAR(50),
-	      DEPARTMENT        CHAR(50)
-	      );"""
+          (
+          ADMISSION INT PRIMARY KEY     NOT NULL,
+          NAME           TEXT    NOT NULL,
+          AGE            INT     NOT NULL,
+          COURSE        CHAR(50),
+          DEPARTMENT        CHAR(50)
+          );"""
 
 data_inserted = [
     "INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3420, 'John', 18, 'Computer Science', 'ICT');",
@@ -98,8 +99,9 @@ data_update = ["UPDATE STUDENT set AGE = 20 where ADMISSION = 3420;"]
 data_delete = ["DELETE from STUDENT where ADMISSION=3420;"]
 
 
-# CreateTable(table_creation) ## Run First
-# InsertData(data_inserted) ## Run Second
-# RetrieveData(retieve_query) ## Run Third
-# UpdateData(data_update, retieve_query) ## Run Fourth
-# DeleteData(data_delete, retieve_query) ## Run Fifth
+driver = PostgresDriver("postgres", "postgres", "zakizakizaki")
+# driver.create_table(table_creation)
+# driver.insert_data(data_inserted)
+# driver.retrieve_data(retieve_query)
+# driver.update_data(data_update, retieve_query)
+# driver.delete_data(data_delete, retrieve_query)
