4d55511103c2c5ae40914c8bfa4697a1b0bdb347
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-11-20 00:01:07 +0800

    Update Cuochdb Functions to a Class

--- leet_code_exercise/couchdb_CRUD.py
+++ leet_code_exercise/couchdb_CRUD.py
@@ -1,60 +1,58 @@
 from cloudant.client import CouchDB
 
 
-def ConnectCouchdb(admin, password):
-    client = CouchDB(admin, password, url="http://0.0.0.0:5984", connect=True)
-    return client
+class CouchDBDriver:
+    def __init__(self, admin, password):
+        self.admin = admin
+        self.password = password
 
+    def connect_couchdb(self, admin, password):
+        client = CouchDB(admin, password, url="http://0.0.0.0:5984", connect=True)
+        return client
 
-def CreateOpenDatabase(name):
-    client = ConnectCouchdb("admin", "password")
-    database = client.create_database(name)
-    if database.exists():
-        print("All Ready Exist")
-        database = client[name]
-        return database
-    else:
+    def create_database(self, name):
+        client = self.connect_couchdb(self.admin, self.password)
         database = client.create_database(name)
         if database.exists():
-            print("Success")
-        return database
+            database = client[name]
+            return database
+        else:
+            database = client.create_database(name)
+            if database.exists():
+                print("Success")
+            return database
 
+    def create_document(self, data, database_name):
+        database = self.create_database(database_name)
+        my_document = database.create_document(data)
+        if my_document.exists():
+            print("SUCCESS!!")
 
-def CreateDocument(data):
-    database = CreateOpenDatabase("test_database")
-    my_document = database.create_document(data)
-    if my_document.exists():
-        print("SUCCESS!!")
+    def retrieve_document(self, data, database_name):
+        database = self.create_database(database_name)
+        my_document = database[data]
+        print(my_document)
 
+    def retieve_all_documents(self, database_name):
+        database = self.create_database(database_name)
+        for document in database:
+            print(document)
 
-def RetrieveDocument(data):
-    database = CreateOpenDatabase("test_database")
-    my_document = database[data]
-    print(my_document)
+    def update_document(self, data, keys, value, database_name):
+        database = self.create_database(database_name)
+        my_document = database[data]
+        for key, val in zip(keys, value):
+            my_document[key] = val
+        my_document.save()
 
-
-def RetieveAllDocuments():
-    database = CreateOpenDatabase("test_database")
-    for document in database:
-        print(document)
-
-
-def UpdateDocument(data, keys, value):
-    database = CreateOpenDatabase("test_database")
-    my_document = database[data]
-    for key, val in zip(keys, value):
-        my_document[key] = val
-    my_document.save()
-
-
-def DeleteDocument(data):
-    database = CreateOpenDatabase("test_database")
-    my_document = database[data]
-    if my_document.exists():
-        my_document.delete()
-        print("Document Deleted")
-    else:
-        print("Document Doesnt Exist")
+    def delete_document(self, data, database_name):
+        database = self.create_database(database_name)
+        my_document = database[data]
+        if my_document.exists():
+            my_document.delete()
+            print("Document Deleted")
+        else:
+            print("Document Doesnt Exist")
 
 
 data = {
@@ -68,7 +66,10 @@ keys = ["name", "age"]
 value = ["Zaki", 21]
 
 
-# CreateDocument(data)
-# RetrieveDocument("testdata")
-# UpdateDocument("testdata", keys, value)
-# DeleteDocument("testdata")
+driver = CouchDBDriver("admin", "password")
+# driver.create_database("test_database")
+# driver.create_document(data, "test_database")
+# driver.retrieve_document("testdata", "test_database")
+# driver.retieve_all_documents("test_database")
+# driver.update_document("testdata", keys, value, "test_database")
+# driver.delete_document("testdata", "test_database")
